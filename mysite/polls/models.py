b30d5d99bf1dabff3f4bea9a48b266e135560ecb
dawid <dawidjasinski1778@gmail.com>
2023-03-14 09:26:50 +0100

    Stworzenie pierwszego obiektu klasy Question przez shell'a

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,7 +8,6 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
     def __str__(self):
         return self.question_text
 
@@ -19,6 +18,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
     def __str__(self):
         return self.choice_text
