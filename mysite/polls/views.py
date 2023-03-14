f304af9115e75fdae74d87ce5a4bcd6f22b51553
dawid <dawidjasinski1778@gmail.com>
2023-03-14 10:48:19 +0100

    Zmiana widoków (Ogólne widoki Django)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,22 +4,26 @@ from django.urls import reverse
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question, Choice
+from django.views import generic
 from django.http import Http404
 from django.template import loader
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
